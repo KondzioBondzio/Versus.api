cd33e5e6623ed56fb808cee9a98d3d8a58edc21b
Kamil Domagała <kamilos3312@gmail.com>
2024-02-20 21:14:15 +0100

    Added shared ApiError model

---
+++ src/Versus.Shared/ApiError.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Text.Json.Serialization;
+
+namespace Versus.Shared;
+
+public class ApiError
+{
+    [JsonPropertyName("type")]
+    public string Type { get; set; } = string.Empty;
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; } = string.Empty;
+
+    [JsonPropertyName("status")]
+    public HttpStatusCode Status { get; set; }
+
+    [JsonPropertyName("detail")]
+    public string Detail { get; set; } = string.Empty;
+}
