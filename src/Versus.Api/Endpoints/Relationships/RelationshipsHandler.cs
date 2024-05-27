a78bc6b28195135b346288c997ced28e5db630b6
Kamil Domagała <kamilos3312@gmail.com>
2024-05-27 19:43:23 +0200

    Replaced RelationshipResponse AutoMapper with manual mapper. Temporary removal of RelationshipRequest

--- src/Versus.Api/Endpoints/Relationships/RelationshipsHandler.cs
+++ src/Versus.Api/Endpoints/Relationships/RelationshipsHandler.cs
@@ -1,35 +1,27 @@
-﻿using System.Security.Claims;
-using AutoMapper.QueryableExtensions;
+﻿using System.Linq.Expressions;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Versus.Api.Data;
+using Versus.Api.Entities;
 using Versus.Api.Extensions;
 using Versus.Shared.Common;
 using Versus.Shared.Relationships;
-using IConfigurationProvider = AutoMapper.IConfigurationProvider;
 
 namespace Versus.Api.Endpoints.Relationships;
 
 public record RelationshipsParameters
 {
-    [FromQuery]
-    public RelationshipsRequest Request { get; init; } = default!;
     public ClaimsPrincipal ClaimsPrincipal { get; init; } = default!;
     public VersusDbContext DbContext { get; init; } = default!;
-    public IConfigurationProvider ConfigurationProvider { get; init; } = default!;
     public CancellationToken CancellationToken { get; init; } = default!;
 
-    public void Deconstruct(out RelationshipsRequest request, 
-        out ClaimsPrincipal claimsPrincipal,
+    public void Deconstruct(out ClaimsPrincipal claimsPrincipal,
         out VersusDbContext dbContext,
-        out IConfigurationProvider configurationProvider,
         out CancellationToken cancellationToken)
     {
-        request = Request;
         claimsPrincipal = ClaimsPrincipal;
         dbContext = DbContext;
-        configurationProvider = ConfigurationProvider;
         cancellationToken = CancellationToken;
     }
 }
@@ -42,19 +34,30 @@ public class RelationshipsHandler : IEndpoint
     public static async Task<Results<Ok<PagedResponse<RelationshipResponse>>, UnauthorizedHttpResult>> HandleAsync
         ([AsParameters] RelationshipsParameters parameters)
     {
-        var (request, claimsPrincipal, dbContext, mapperConfig, cancellationToken) = parameters;
+        var (claimsPrincipal, dbContext, cancellationToken) = parameters;
 
         var userId = claimsPrincipal.GetUserId();
 
         var relationships = await dbContext.UserRelationships
             .AsNoTracking()
             .Where(x => x.UserId == userId || x.RelatedUserId == userId)
-            .Skip((request.PageNumber - 1) * request.PageSize ?? 0)
-            .Take(request.PageSize ?? 10)
-            .ProjectTo<RelationshipResponse>(mapperConfig)
+            // .Skip((request.PageNumber - 1) * request.PageSize ?? 0)
+            // .Take(request.PageSize ?? 10)
+            .Select(Map(userId))
             .ToListAsync(cancellationToken);
 
         var result = new PagedResponse<RelationshipResponse>(0, relationships.Count, relationships.Count, relationships);
         return TypedResults.Ok(result);
     }
+
+    private static Expression<Func<UserRelationship, RelationshipResponse>> Map(Guid userId) => src => new RelationshipResponse
+    {
+        Id = src.Id, 
+        UserId = src.UserId == userId ? src.RelatedUserId : src.UserId, 
+        Direction = src.UserId == userId
+            ? RelationshipResponse.RelationshipDirection.Outgoing
+            : RelationshipResponse.RelationshipDirection.Incoming,
+        Status = (int)src.Status, 
+        Type = (int)src.Type
+    };
 }
